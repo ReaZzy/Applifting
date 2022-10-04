e963e9642f4b51edeb72c340fc2172639c64a678
ReaZzy <maxim.nebela@gmail.com>
2022-10-04 20:25:05 +0200

    chore🚕: add jest support

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  moduleDirectories: ['node_modules', __dirname],
+  moduleNameMapper: {
+    '^@src(.*)$': '<rootDir>/src$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+};
