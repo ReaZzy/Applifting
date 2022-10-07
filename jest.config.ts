191bcb9098ff7c87e72ec1359f78c4a63494bf93
ReaZzy <maxim.nebela@gmail.com>
2022-10-07 14:29:43 +0200

    chore🚕: add file transformer

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,13 @@ export default {
   moduleDirectories: ['node_modules', __dirname],
   moduleNameMapper: {
     '^@src(.*)$': '<rootDir>/src$1',
-    '^@public(.*)$': '<rootDir>/public',
+    '^@public(.*)$': '<rootDir>/public$1',
   },
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/utils/fileTransformer.js',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
